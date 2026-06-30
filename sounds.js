2c116ea93d8dc6a87711553f303b3f3ff833bbe9
TurkrosoftLabs <qntkjz42n7@privaterelay.appleid.com>
2026-07-01 00:03:23 +0300

    Add files via upload

---
+++ sounds.js
@@ -0,0 +1,12 @@
+const sounds = [
+    "Birds and distant traffic.wav",
+    "Rain on beach.wav",
+    "Wind.wav",
+    "Polar.wav",
+    "Fireplace.wav",
+    "Air conditioner.wav",
+    "Desk fan.wav",
+    "Car.wav",
+    "Airplane.wav",
+    "Aliens.wav"
+]
