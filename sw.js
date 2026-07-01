609069ea23c84d6b4d54e5cfe4df48f958f5e863
TurkrosoftLabs <qntkjz42n7@privaterelay.appleid.com>
2026-07-01 12:46:08 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,60 @@
+const CACHE_NAME = "ambient-player-v1";
+
+const APP_FILES = [
+    "./",
+    "./index.html",
+    "./sounds.js"
+];
+
+// Install
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll(APP_FILES);
+        })
+    );
+
+    self.skipWaiting();
+});
+
+// Activate
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        self.clients.claim()
+    );
+});
+
+// Fetch
+self.addEventListener("fetch", event => {
+
+    // Only GET requests
+    if (event.request.method !== "GET")
+        return;
+
+    event.respondWith(
+
+        caches.match(event.request).then(async cached => {
+
+            if (cached)
+                return cached;
+
+            const response = await fetch(event.request);
+
+            // Cache audio files only after they're requested
+            if (
+                event.request.url.endsWith(".wav")
+            ) {
+
+                const cache = await caches.open(CACHE_NAME);
+
+                cache.put(event.request, response.clone());
+
+            }
+
+            return response;
+
+        })
+
+    );
+
+});
